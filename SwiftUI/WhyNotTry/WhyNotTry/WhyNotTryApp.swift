e78a4669ed0355c32f9e9bbc13674af119a7d444
SeckMohameth <mohamethseck34@gmail.com>
2024-01-19 18:01:05 -0500

    adding WhyNotTry SwiftUI project guide

---
+++ SwiftUI/WhyNotTry/WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Mohameth Seck on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

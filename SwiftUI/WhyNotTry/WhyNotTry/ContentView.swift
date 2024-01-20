00603a8236bf7749d0f15fdd81b3b0454685d3b9
SeckMohameth <mohamethseck34@gmail.com>
2024-01-20 00:12:30 -0500

    adding WhyNotTry App

--- SwiftUI/WhyNotTry/WhyNotTry/ContentView.swift
+++ SwiftUI/WhyNotTry/WhyNotTry/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+    
+    @State private var selected = "Baseball"
+    @State private var id = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            
+            Text("Why not try...")
+                .font(.largeTitle.bold())
+            
+            Spacer()
+            
+            VStack {
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                            .font(.system(size: 144))
+                            .foregroundStyle(.white)
+                )
+                
+                Text("\(selected)!")
+                    .font(.title)
+                
+            }// end of nested VStack
+            .transition(.slide)
+            .id(id)
+            
+            Spacer()
+            
+            Button("Try Again") {
+                withAnimation(.easeInOut(duration: 1)) {
+                    selected = activities.randomElement() ?? "Archery"
+                    id += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
+        }//END of VStack
+       
     }
 }
 

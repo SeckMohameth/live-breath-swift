c3a4d62dff5fde8564bb9e06d442102dbb2fba10
SeckMohameth <mohamethseck34@gmail.com>
2024-01-23 22:01:05 -0500

    practicing Lists

---
+++ SwiftUI/Lists/Lists/ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsApp.swift
+//  Lists
+//
+//  Created by Mohameth Seck on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

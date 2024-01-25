4864959e4d815a40ba59b98b2af647ad8dbec3af
SeckMohameth <mohamethseck34@gmail.com>
2024-01-24 23:19:52 -0500

    working with closures

--- Swift/Closures.playground/Contents.swift
+++ Swift/Closures.playground/Contents.swift
@@ -1,3 +1,28 @@
 import Foundation
 
 
+struct Student {
+    let name: String
+    var testScores: Int
+}
+
+
+let students = [
+    Student(name: "Mo", testScores: 100),
+    Student(name: "Dom", testScores: 85),
+    Student(name: "Alex", testScores: 79),
+    Student(name: "Kay", testScores: 70)
+
+]
+
+
+//closure
+var topStudentFilter: (Student) -> Bool = { student in
+    return student.testScores > 80
+    
+}
+
+//fuction
+func topStudentFilterF(student: Student) -> Bool {
+    return student.testScores > 70
+}

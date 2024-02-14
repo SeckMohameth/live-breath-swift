a4796f76944fa212c98eb5cebe365a60df174d82
SeckMohameth <mohamethseck34@gmail.com>
2024-02-13 19:43:11 -0500

    practicing forms

---
+++ SwiftUI/FormsPractice/FormsPractice/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FormsPractice
+//
+//  Created by Mohameth Seck on 2/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/FormsPractice/FormsPractice/FormsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormsPracticeApp.swift
+//  FormsPractice
+//
+//  Created by Mohameth Seck on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FormsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

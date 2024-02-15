d00004bf425e3ca63c2a3b723518aef2e52f27fa
SeckMohameth <mohamethseck34@gmail.com>
2024-02-14 23:45:08 -0500

    trying different form views

--- SwiftUI/FormsPractice/FormsPractice/ContentView.swift
+++ SwiftUI/FormsPractice/FormsPractice/ContentView.swift
@@ -10,12 +10,32 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var birthdate = Date()
+    @State private var shouldSendNewsLetter = false
+    @State private var numberOfLikes = 1
     
     var body: some View {
-        VStack {
-            Form {
-                TextField("Firs Name", text: $firstName)
+        NavigationView {
+            VStack {
+                    Form {
+                        Section(header: Text("personal information")) {
+                        TextField("Firs Name", text: $firstName)
+                        TextField("Last Name", text: $lastName)
+                            DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    }
+                        Section(header: Text("Actions")) {
+                            Toggle("Send Newsletter", isOn: $shouldSendNewsLetter)
+                                .toggleStyle(SwitchToggleStyle(tint: .red))
+                            Stepper("Number of likes", value: $numberOfLikes, in: 1...100)
+                            Text("This video has \(numberOfLikes) likes")
+                            Link("Terms of Service", destination: URL(string: "https//mohamethseck.com")!)
+                        }
+                }
+                    
             }
+            .accentColor(.red)
+            .navigationTitle("Account")
         }
     }
 }

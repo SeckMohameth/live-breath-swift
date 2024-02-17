a4819ed048e3af1d308feb3e7848ad5d6605626a
SeckMohameth <mohamethseck34@gmail.com>
2024-02-17 18:17:31 -0500

    learning swift data

--- SwiftDataDemo/SwiftDataDemo/CarData.swift
+++ SwiftDataDemo/SwiftDataDemo/CarData.swift
@@ -5,4 +5,18 @@
 //  Created by Mohameth Seck on 2/17/24.
 //
 
+// https://www.youtube.com/watch?v=krRkm8w22A8&t=777s&ab_channel=CodeWithChris
+
 import Foundation
+import SwiftData
+
+@Model
+class DataItem: Identifiable {
+    var id: String
+    var name: String
+    
+    init(name: String) {
+        self.id = UUID().uuidString
+        self.name = name
+    }
+}
--- SwiftDataDemo/SwiftDataDemo/ContentView.swift
+++ SwiftDataDemo/SwiftDataDemo/ContentView.swift
@@ -6,17 +6,72 @@
 //
 
 import SwiftUI
+import SwiftData
+
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) private var context
+    
+    // retrieve an array of instances of DataItem
+    @Query private var items: [DataItem]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
+            Text("Tap to add car data")
+            Button("Add item"){
+                addItem()
+            }
+            
+            List {
+                ForEach (items) { item in
+                    HStack {
+                        Text(item.name)
+                        Spacer()
+                        Button {
+                            updateItem(item)
+                        } label: {
+                            Image(systemName: "pencil")
+                        }
+                       
+                    }
+                }
+                .onDelete { indexes in
+                    for index in indexes {
+                        deleteItem(items[index])
+                    }
+                }
+            }
         }
         .padding()
     }
+    
+    // =============== functions ================
+    
+    func addItem() {
+        // create the item
+        let item = DataItem(name: "toyota")
+        
+        
+        // add item to data context
+        context.insert(item)
+    }
+    
+    func deleteItem(_ item: DataItem) {
+        context.delete(item)
+    }
+    
+    func updateItem(_ item: DataItem){
+        //edit item data
+        item.name = "updated test item"
+        
+        // then save the context
+        try? context.save()
+    }
+    
+    // =============== functions ================
+    
 }
 
 #Preview {

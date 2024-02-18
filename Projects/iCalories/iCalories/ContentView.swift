23825f935034afeb8ddf5ce2616e9954346a7426
SeckMohameth <mohamethseck34@gmail.com>
2024-02-18 17:52:07 -0500

    practicing core data

---
+++ Projects/iCalories/iCalories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iCalories
+//
+//  Created by Mohameth Seck on 2/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projects/iCalories/iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Mohameth Seck on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
